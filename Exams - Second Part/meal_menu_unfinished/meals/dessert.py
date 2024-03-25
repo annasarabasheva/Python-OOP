8fce1e3829c98719e3576054395a0947096a9154
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-03-25 17:39:37 +0200

    Delete Exams - Second Part directory

--- Exams - Second Part/meal_menu_unfinished/meals/dessert.py
+++
@@ -1,9 +0,0 @@
-from meal_menu_unfinished.meals.meal import Meal
-
-
-class Dessert(Meal):
-    def __init__(self, name, price, quantity=30):
-        super().__init__(name, price, quantity)
-
-    def details(self):
-        return f"Dessert {self.name}: {self.price:.2f}lv/piece"
--- Exams - Second Part/meal_menu_unfinished/test.py
+++
@@ -1,6 +0,0 @@
-value = input()
-
-if value[0] != '0' or len(value) != 10 or not value.isnumeric():
-    raise ValueError("Invalid phone number!")
-else:
-    print('ok')
