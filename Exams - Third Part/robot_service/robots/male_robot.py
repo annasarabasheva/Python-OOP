1247f66144ad9b66c0cb4ba20980827d5e9b596c
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-03-27 19:23:46 +0200

    Delete Exams - Third Part directory

--- Exams - Third Part/robot_service/robots/male_robot.py
+++
@@ -1,9 +0,0 @@
-from robot_service.robots.base_robot import BaseRobot
-
-
-class MaleRobot(BaseRobot):
-    def __init__(self, name: str, kind: str, price: float):
-        super().__init__(name, kind, price, weight=9)
-
-    def eating(self):
-        self.weight += 3
